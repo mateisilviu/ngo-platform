28afd0fcb251647e77e58c64558de95bbef6cd43
Silviu Matei <tzevy.cont@gmail.com>
2023-03-19 15:29:25 +0200

    adding homepage image carusel

--- ngoclient/lib/ui/homepage.dart
+++ ngoclient/lib/ui/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:ngoclient/core/util/responsive.dart';
 import 'package:ngoclient/dashboard/constants/responsive.dart';
@@ -9,7 +10,11 @@ import 'footer/footer.dart';
 import 'screens/categories_screen.dart';
 
 class HomePageScreen extends StatelessWidget {
-  const HomePageScreen({super.key});
+  var homepageImages = [
+    Image(image: AssetImage('images/homepage/donate1_1024.jpg')),
+    Image(image: AssetImage('images/homepage/donate2_1024.jpg')),
+    Image(image: AssetImage('images/homepage/donate3_1024.jpg')),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -60,7 +65,27 @@ class HomePageScreen extends StatelessWidget {
         ]),
         body: SingleChildScrollView(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 800,
+                  child: CarouselSlider(
+                    //carouselController: _controller,
+                    options: CarouselOptions(
+                        pauseAutoPlayOnManualNavigate: false,
+                        pauseAutoPlayOnTouch: false,
+                        viewportFraction: 1.0,
+                        autoPlay: true,
+                        autoPlayInterval: Duration(seconds: 4),
+                        autoPlayAnimationDuration: Duration(seconds: 1),
+                        autoPlayCurve: Curves.slowMiddle,
+                        aspectRatio: 1),
+                    items: homepageImages,
+                  ),
+                ),
+              ),
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.6,
               ),
